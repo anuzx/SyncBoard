6f74c24f4289d80d2a053d5c60136a8fb2e256fe
anuzx <anujparasar4266@gmail.com>
2025-11-28 04:15:10 +0530

    inital setup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,35 @@
 import express from "express"
-
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "./config";
+import { userAuth } from "./middleware";
 const app = express()
 
-app.listen(3000 , ()=> console.log("server started at port 3000"))
+
+
+app.post("/signup", (req, res) => {
+    //we should do zod validation here
+})
+
+
+app.post("/signin", (req, res) => {
+  
+    const userId = 1;
+    const token =jwt.sign({
+    userId
+    }, JWT_SECRET)
+    
+    res.json({
+        token
+    })
+});
+
+
+
+app.post("/createRoom", userAuth ,(req, res) => {
+  
+});
+
+
+app.listen(3001, () => console.log("server started at port 3000"))
+
+
---
+++ learning.md
@@ -0,0 +1,9 @@
+# Learning
+
+1.) for pnpm we use "workspace:*" for npm we use only "*" , * means picking from same package
+
+```
+"devDependencies": {
+    "@repo/typescript-config": "workspace:*",
+}
+```
